4073a790ac79c2e5d78c5ecf6963d72ccb8bba00
Kevin Gallagher <kevinjgallagher@gmail.com>
2021-01-25 22:39:21 +0000

    initial project setup

---
+++ setup.py
@@ -0,0 +1,48 @@
+import setuptools
+import os
+import unittest
+from typing import Dict, List, Set, Tuple
+
+from os.path import dirname
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+my_dir = dirname(__file__)
+
+def binder_test_suite() -> unittest.TestSuite:
+    """Test suite for Binder tests"""
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover(os.path.join(my_dir, 'tests'), pattern='test_*.py')
+    return test_suite
+
+dev = [
+    'black',
+    'flake8',
+    'flake8-colors',
+    'wheel',
+]
+
+EXTRAS_REQUIREMENTS: Dict[str, List[str]] = {
+    'dev': dev,
+}
+
+setuptools.setup(
+    name="binder",
+    version="0.0.1",
+    extras_require=EXTRAS_REQUIREMENTS,
+    install_requires=[
+        'pyaml',
+    ],
+    description="A scrap attachment application.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    test_suite='setup.binder_test_suite',
+)
